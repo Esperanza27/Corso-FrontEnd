6d2f1c7463068a37f7de000229e9e25a7aba8905
Esperanza <esperanza.llontop27@gmail.com>
2023-11-02 14:58:27 +0100

    esercizio  G3

--- settDueJs/G3/app/assets/app.js
+++ settDueJs/G3/app/assets/app.js
@@ -1,42 +1,52 @@
-/* ricevere attraverso un form il nome e l'ano di nascita, calcolare 
-si la persona e maggiorenne o minorenne, salutarla indicando la maggiore*/
+/* ricevere attraverso un form il nome e l'ano di nascita, calcolare si la persona e maggiorenne o minorenne, salutarla indicando la maggiore*/
 /* 
  1. LEGGERE IL CAMPO NOME E MEMORIZZARLO
  2. LEGGERE IL CAMPO ANNO E MEMORIZZARLO
- 3. CALCOLARE L'ETà
- 4. VERIFICA LA MAGGIORE O MINORE ETà
- 5. SCRIVERE NELL'HTML
- 6. CANCELLARE IL FORM 
+ 3. Controllare la validità del form
+ 4.CALCOLARE L'ETà
+ 5. VERIFICA LA MAGGIORE O MINORE ETà
+ 6. SCRIVERE NELL'HTML
+ 7. CANCELLARE IL FORM 
 
- EVENTO SCATENANTE
- click
+. EVENTO SCATENANTE
+    click
 
- VARIABILI GLOBALI
- nome;
- anno;
- stato
+ . VARIABILI GLOBALI
+    nome;
+    anno;
+    stato
   */
 let nome;
-/* globale in quanto è utilizzata  per leggere il campo di input per stampare 
- a video  */
+/* globale in quanto è utilizzata  per leggere il campo di input per stampare a video  */
 let anno;
 let eta;
 let stato;
-/* globale in quanto è utilizzata per verificare 
- l'età e stampare a video  */
- let errore = document.getElementById("errore");
+/* globale in quanto è utilizzata per verificare l'età e stampare a video  */
+let errore = document.getElementById("errore");
+let esegui = false;
 function eventHandler() {
   leggiForm();
-  if (anno >= 1990 && anno <= 2023 && nome != '' ) {
-    errore.innerHTML='';
+  controlla();
+  if (esegui) {
     calcolaEta();
     verifica();
     scrivi();
     cancellaForm();
   } else {
-    errore.innerHTML= 'Attenzione! compilare tutti i campi e inserire un numero tra 1900 e 2023 ';
+    errore.innerHTML =
+      "Attenzione! compilare tutti i campi e inserire un numero tra 1900 e 2023! ";
     return;
   }
+}
+function controlla() {
+  esegui = false;
+  if (anno >= 1990 && anno <= 2023) {
+    if (nome != "") {
+      errore.innerHTML = "";
+      esegui = true;
+    }
+  }
+  return esegui;
 }
 function leggiForm() {
   nome = document.getElementById("nome").value;
@@ -52,8 +62,11 @@ function verifica() {
   console.log(stato);
 }
 function scrivi() {
-
+  document.getElementById("mioNome").innerHTML = "ciao " + nome;
+  document.getElementById("miaVerifica").innerHTML =
+    "la tua età è " + eta + "; sei " + stato;
 }
 function cancellaForm() {
-    
+  document.getElementById("nome").value = "";
+  document.getElementById("anno").value = "";
 }
