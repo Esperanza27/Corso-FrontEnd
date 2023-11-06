c3800d19582fd96d4fed73933f1db22c92f39197
Esperanza <esperanza.llontop27@gmail.com>
2023-11-06 18:14:53 +0100

    esercizioJs

--- settDueJs/progettoSettimanale2/eserciziJs/assets/D5.js
+++ settDueJs/progettoSettimanale2/eserciziJs/assets/D5.js
@@ -74,32 +74,44 @@ model: "208",
 color: "blue",
 trims: ["allure", "GT"],})
 console.log("questo è il esercizio 6", cars)
-/* function modifyCar() {
-  const newCar = [];
-  for (let index = 0; index < cars.length; index++) {
-    cars[index].trims.pop()
-   newCar.push({...cars[index], trims: cars })
-  }
-  return newCar;
- }      
- console.log(modifyCar()); */
+for (let index = 0; index < array.length; index++) {
+  cars[index].trims.pop();
+  
+}
+console.log("esercio 6", cars);
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = [];
+for (let index = 0; index < cars.length; index++) {
+  justTrims.push(cars[index].trims[0]);
+  
+}
+console.log(justTrims)
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
-
+/* for (let index = 0; index < array.length; index++) {
+ if (cars[index].color ) {
+ } else {
+  
+ }
+  
+} */
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
+let conta = 0;
+while (numericArray[0]!== 32) {
+  console.log(numericArray[0]);
+  conta++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
