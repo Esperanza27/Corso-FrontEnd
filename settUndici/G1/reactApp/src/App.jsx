167e2308e0ac76ad575265088e60f3ebc29168a7
Esperanza <esperanza.llontop27@gmail.com>
2024-01-08 18:28:49 +0100

    modify the button folder

--- settUndici/G1/reactApp/src/App.jsx
+++ settUndici/G1/reactApp/src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import ButtonComponent from "./components/buttons/ButtonComponent";
 import ImageComponent from "./components/images/ImageComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
   const initialCounter = 0;
-
   return (
     <>
       <div>
@@ -20,7 +16,7 @@ function App() {
       </div>
 
       <div className="card">
-        <ButtonComponent label="👍" score={initialCounter} />
+        <ButtonComponent label="👍" score={initialCounter} bgColor = "gray" />
       </div>
     </>
   );
--- settUndici/G1/reactApp/src/components/buttons/ButtonComponent.jsx
+++ settUndici/G1/reactApp/src/components/buttons/ButtonComponent.jsx
@@ -2,30 +2,9 @@ import { useState } from "react"
 
 const ButtonComponent = (props) =>{
     const [count, setCount] = useState(props.score)
-return <button style={{marginTop: '25px'}} onClick={() => setCount((count) => count +1)}>{props.label}{count}</button>
+    const customStyle ={
+      marginTop: '15px'
+    }
+return <button style={{backgroundColor: props.bgColor || "", ...customStyle}} onClick={() => setCount((count) => count +1)}>{`${props.label } ${count}` || 'button'}</button>
 }
 export default ButtonComponent
-
-/* import { useState } from "react"
-
-const Button = (props) => {
-
-  const [count, setCount] = useState(props.pippo);
-
-  // COME STILIZZARE UN COMPONENTE IN REACT
-  // RICORDARSI CHE NON SI UTILIZZANO TRATTINI MA CAMEL CASE
-   const stileComp = {
-      marginTop: '150px',
-      paddingLeft: '50px',
-   }
-
-    return (
-      // NEL COMPONENTE POI DEVO PASSARE LO STYLE TRA GRAFFE
-      // SE LO VOLESSI FARE IN-LINE DEVO METTERE 2 GRAFFE {{marginTop: '150px'}}
-        <button style={{marginTop: '150px'}} onClick={() => setCount((count) => count +1)} >
-          Conteggio = {count}
-        </button>
-    )
-}
-
-export default Button */
