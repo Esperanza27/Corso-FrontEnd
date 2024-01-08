901aac4d370843fda34cd5334ac758a77dff15e4
Esperanza <esperanza.llontop27@gmail.com>
2024-01-08 17:35:27 +0100

    I shaded the carpet

--- settUndici/G1/reactApp/src/App.jsx
+++ settUndici/G1/reactApp/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ButtonComponent from "./components/buttons/ButtonComponent";
-import ImageComponent from "./components/image/ImageComponent";
+import ImageComponent from "./components/images/ImageComponent";
 
 function App() {
   const [count, setCount] = useState(0);
